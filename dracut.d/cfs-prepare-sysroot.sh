0730e87661576154563837dffa7f71ec1f5b3651
Shawn Wang <shawn111@gmail.com>
2024-11-03 22:40:36 +0800

    copy etc in build time

--- dracut.d/cfs-prepare-sysroot.sh
+++ dracut.d/cfs-prepare-sysroot.sh
@@ -21,11 +21,4 @@ mount --bind /sysroot.tmp/etc /sysroot/etc
 test -d /sysroot.tmp/ostree/deploy/default/var && mount --bind /sysroot.tmp/ostree/deploy/default/var /sysroot/var || mount --bind /sysroot.tmp/var /sysroot/var
 
 
-## FIXME
-for i in fstab passwd shadow
-do
-test -f /sysroot/ostree/deploy/default/deploy/342d825130840afed290183be9e00a0e03907502bcb1f16cc60f92643924ed32.0/etc/$i && cp  /sysroot/ostree/deploy/default/deploy/342d825130840afed290183be9e00a0e03907502bcb1f16cc60f92643924ed32.0/etc/$i /sysroot/etc/$i
-done
-#mount --bind /sysroot.tmp/ostree/deploy/default/deploy/37595a2f96fc23131eef6af87920858e6eecc4de5540ef3278aa7e184c7d4d5c.0/etc /sysroot/etc
-#mount --bind /sysroot.tmp/ostree/deploy/default/var /sysroot/var
 
