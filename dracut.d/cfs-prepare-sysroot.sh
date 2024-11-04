2459cabfb029afe96000840b29a02711ea19289c
Shawn Wang <shawn111@gmail.com>
2024-11-04 09:59:26 +0800

    use . instead source in dracut

--- dracut.d/cfs-prepare-sysroot.sh
+++ dracut.d/cfs-prepare-sysroot.sh
@@ -18,13 +18,12 @@ mount -o rw $root /sysroot.tmp
 ## FIXME image name
 mount -t composefs /sysroot.tmp/composefs/images/$image.cfs -o basedir=/sysroot/composefs/repo /sysroot
 
-source /sysroot/usr/lib/os-release
 # ID
 # VERSION_ID
-
+. /sysroot/usr/lib/os-release || true
 ## init
 #test -d /sysroot.tmp/composefs/etc/$ID.$VERSION_ID || cp -rpf /sysroot/usr/etc /sysroot.tmp/composefs/etc/$ID.$VERSION_ID
+mount --bind /sysroot.tmp/composefs/etc/$ID.$VERSION_ID  /sysroot/etc
 
 ## FIXME var / etc
-mount --bind /sysroot.tmp/composefs/etc/$ID.$VERSION_ID  /sysroot/etc
 test -d /sysroot.tmp/ostree/deploy/default/var && mount --bind /sysroot.tmp/ostree/deploy/default/var /sysroot/var || mount --bind /sysroot.tmp/var /sysroot/var
